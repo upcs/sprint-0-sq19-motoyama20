74aafe854f45efceec8908a41e0f94eab573b975
motoyama20 <35584862+motoyama20@users.noreply.github.com>
2019-01-28 19:48:37 -0800

    Add second unit test
    To test sum AND difference of the two numbers

--- js/functions.test.js
+++ js/functions.test.js
@@ -1,5 +1,9 @@
 const sum = require('./functions');
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3);
+test('adds 2 + 5 to equal 7', () => {
+  expect(sum(2, 5)).toBe(7);
+});
+
+test('adds 2 + 5 to equal 7', () => {
+  expect(difference(2, 5)).toBe(-3);
 });
