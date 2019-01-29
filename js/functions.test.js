dcd500da37cefd80582eaab6b4c58d77e26e3635
motoyama20 <35584862+motoyama20@users.noreply.github.com>
2019-01-28 20:05:20 -0800

    Minor Comment changes

--- js/functions.test.js
+++ js/functions.test.js
@@ -4,6 +4,6 @@ test('adds 2 + 5 to equal 7', () => {
   expect(sum(2, 5)).toBe(7);
 });
 
-test('adds 2 + 5 to equal 7', () => {
+test('subtracts 2 - 5 to equal -3', () => {
   expect(difference(2, 5)).toBe(-3);
 });
