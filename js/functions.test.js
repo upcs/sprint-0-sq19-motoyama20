bb4cfa9a8faa5720c80d5dc43cc033867db9e073
motoyama20 <35584862+motoyama20@users.noreply.github.com>
2019-01-29 09:29:42 -0800

    Change to fail

--- js/functions.test.js
+++ js/functions.test.js
@@ -1,7 +1,7 @@
 const sum = require('./functions');
 
-test('adds 2 + 5 to equal 7', () => {
-  expect(sum(2, 5)).toBe(7);
+test('adds 2 + 5 to equal 8', () => {
+  expect(sum(2, 5)).toBe(8);
 });
 
 test('multiply sum of 2 + 5 to equal 14', () => {
