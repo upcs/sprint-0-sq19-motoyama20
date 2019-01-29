21b71b054ed651031dce232a2f66c4428d6419da
motoyama20 <35584862+motoyama20@users.noreply.github.com>
2019-01-29 09:08:22 -0800

    test is a special word

--- js/functions.test.js
+++ js/functions.test.js
@@ -1,9 +1,9 @@
 const sum = require('./sum');
 
-testOne('adds 2 + 5 to equal 7', () => {
+test('adds 2 + 5 to equal 7', () => {
   expect(sum(2, 5)).toBe(7);
 });
 
-testTwo('multiply sum of 2 + 5 to equal 14', () => {
+test('multiply sum of 2 + 5 to equal 14', () => {
   expect((sum(2, 5))*2).toBe(14);
 });
